164e1d7f13fd45c79a6a0bf220f1612964aff8ba
a1ecsss <estebana.delapena@gmail.com>
2025-02-13 19:42:59 -0600

    implementacion de tiempo cronometrado

--- Main.java
+++ Main.java
@@ -3,51 +3,61 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // Generar un archivo con 3000 números aleatorios
-        Generator.generarArchivo(3000);
-
-        // Leer los números desde el archivo y almacenarlos en una lista
-        List<Integer> numeros = Generator.leerArchivo();
-
-        // Verificar si la lista está vacía
-        if (numeros.isEmpty()) {
-            System.out.println("No se encontraron números en el archivo.");
-            return;
+        //List<Integer> tamaños = Arrays.asList(500, 1000, 1500, 2000, 2500, 3000);
+        List<Integer> tamaños = new ArrayList<>();
+        for (int i = 300; i <= 3000; i += 300) {
+            tamaños.add(i);
         }
 
-        // Imprimir los números leídos para referencia
-        //System.out.println("Números leídos del archivo: " + numeros);
 
-        // Aplicar y mostrar diferentes algoritmos de ordenamiento
-        List<Integer> copiaNumeros;
+        for (int i = 0; i < tamaños.size(); i++) {
+            Generator.generarArchivo(tamaños.get(i));
+            
+            
+            // Leer los números desde el archivo y almacenarlos en una lista
+            List<Integer> numeros = Generator.leerArchivo();
 
-        // Insertion Sort
-        copiaNumeros = new ArrayList<>(numeros);
-        //System.out.println("\nOrdenamiento con Insertion Sort:");
-        Sorts.insertionSort(copiaNumeros);
-        //System.out.println(copiaNumeros);
+            // Verificar si la lista está vacía
+            if (numeros.isEmpty()) {
+                System.out.println("No se encontraron números en el archivo.");
+                return;
+            }
+            System.out.println("\nTiempos para: " + tamaños.get(i) +" numeros");
+            // Imprimir los números leídos para referencia
+            //System.out.println("Números leídos del archivo: " + numeros);
 
-        // Merge Sort
-        copiaNumeros = new ArrayList<>(numeros);
-        //System.out.println("\nOrdenamiento con Merge Sort:");
-        Sorts.mergeSort(copiaNumeros);
-        //System.out.println(copiaNumeros);
+            // Aplicar y mostrar diferentes algoritmos de ordenamiento
+            List<Integer> copiaNumeros;
 
-        // Quick Sort
-        copiaNumeros = new ArrayList<>(numeros);
-        //System.out.println("\nOrdenamiento con Quick Sort:");
-        Sorts.quickSort(copiaNumeros, 0, copiaNumeros.size() - 1);
-        //System.out.println(copiaNumeros);
+            // Insertion Sort
+            copiaNumeros = new ArrayList<>(numeros);
+            //System.out.println("\nOrdenamiento con Insertion Sort:");
+            Sorts.insertionSort(copiaNumeros);
+            //System.out.println(copiaNumeros);
 
-        // Radix Sort
-        copiaNumeros = new ArrayList<>(numeros);
-        //System.out.println("\nOrdenamiento con Radix Sort:");
-        Sorts.radixSort(copiaNumeros);
-        //System.out.println(copiaNumeros);
+            // Merge Sort
+            copiaNumeros = new ArrayList<>(numeros);
+            //System.out.println("\nOrdenamiento con Merge Sort:");
+            Sorts.mergeSort(copiaNumeros);
+            //System.out.println(copiaNumeros);
 
-        // Heap Sort
-        copiaNumeros = new ArrayList<>(numeros);
-        //System.out.println("\nOrdenamiento con Heap Sort:");
-        Sorts.heapSort(copiaNumeros);
-        //System.out.println(copiaNumeros);
+            // Quick Sort
+            copiaNumeros = new ArrayList<>(numeros);
+            //System.out.println("\nOrdenamiento con Quick Sort:");
+            Sorts.quickSort(copiaNumeros, 0, copiaNumeros.size() - 1);
+            //System.out.println(copiaNumeros);
+
+            // Radix Sort
+            copiaNumeros = new ArrayList<>(numeros);
+            //System.out.println("\nOrdenamiento con Radix Sort:");
+            Sorts.radixSort(copiaNumeros);
+            //System.out.println(copiaNumeros);
+
+            // Heap Sort
+            copiaNumeros = new ArrayList<>(numeros);
+            //System.out.println("\nOrdenamiento con Heap Sort:");
+            Sorts.heapSort(copiaNumeros);
+            //System.out.println(copiaNumeros);
+        }
     }
 }
