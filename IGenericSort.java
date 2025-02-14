8fb8c91a0e4a2bd2d43d0cf4bb86b2ee36772877
a1ecsss <estebana.delapena@gmail.com>
2025-02-13 19:25:40 -0600

    implementacion de tiempo cronometrado

--- IGenericSort.java
+++
@@ -1,5 +0,0 @@
-import java.lang.Comparable;
-
-public interface IGenericSort<T extends Comparable<T>>{ //Límito el código, el tipo de dato tiene que poder ser comparable.
-    public T[] sort(T[] arr);
-}
